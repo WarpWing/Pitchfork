0b49c951fe5ce39a6aaa0365e6d49d5d5790759d
Ty Chermsirivatana <chermsit@dickinson.edu>
2024-02-19 15:43:38 -0500

    Update pitchfork.py

--- src/menus/pitchfork.py
+++ src/menus/pitchfork.py
@@ -11,19 +11,6 @@ from selenium.webdriver.chrome.options import Options
 from typing_extensions import *
 
 def find_week():
-    today = datetime.now()
-    with open(os.path.join('src', 'schedule.csv'), 'r') as csvfile:
-        rows = list(csv.DictReader(csvfile))
-        for i in range(len(rows)):
-            start_date_str = rows[i]['Date']
-            start_date = datetime.strptime(f"{today.year}/{start_date_str}", '%Y/%m/%d')
-            if i < len(rows) - 1:
-                end_date_str = rows[i + 1]['Date']
-                end_date = datetime.strptime(f"{today.year}/{end_date_str}", '%Y/%m/%d')
-            else:
-                end_date = datetime(today.year + 1, 1, 1)
-            if start_date <= today < end_date:
-                return rows[i]['Week']
     return f"Meal for {datetime.now().strftime('%A, %B %d')}"
 
 def get_menu_from_website():
